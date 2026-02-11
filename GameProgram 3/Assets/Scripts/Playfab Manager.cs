6bf842b270a6d3296406f3d579c90d5849a1a002
tgahn04 <tgahn0524@gmail.com>
2026-02-11 19:45:34 +0900

    (2026.2.11) 포톤 네트워크

---
+++ GameProgram 3/Assets/Scripts/Playfab Manager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField addressInputField;
+    [SerializeField] InputField passwordInputField;
+    [SerializeField] string version;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = version;
+
+        StartCoroutine(ConnectRoutine());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.LoadLevel("Lobby");
+    }
+
+    private IEnumerator ConnectRoutine()
+    {
+        // Name Server에서 Master Server로 넘어가는 중...
+        PhotonNetwork.ConnectUsingSettings();
+
+        // 서버 연결이 완료되거나 시간 초과가 될 때까지 대기합니다.
+        while (PhotonNetwork.IsConnectedAndReady == false)
+        {
+            yield return null;
+        }
+
+        // 특정 로비를 생성하여 진입하는 함수
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = addressInputField.text,
+            Password = passwordInputField.text,
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Success,
+            Failure
+        );
+    }    
+
+    public void Subscribe()
+    {
+        Debug.Log("Create Account");
+    }
+
+    public void Failure(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+}
