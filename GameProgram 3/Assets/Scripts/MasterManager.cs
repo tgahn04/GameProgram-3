3d351e07e2f5431632b0cda953e55722fd3c63be
tgahn04 <tgahn0524@gmail.com>
2026-03-05 19:45:26 +0900

    (2026.3.5) 지연 보장 시스템

---
+++ GameProgram 3/Assets/Scripts/MasterManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System.Collections;
+using Photon.Realtime;
+
+public class MasterManager : MonoBehaviourPunCallbacks
+{
+    private WaitForSeconds waitForSeconds = new WaitForSeconds(5);
+
+    public IEnumerator Start()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate("Potion", Vector3.zero, Quaternion.identity);
+
+            yield return waitForSeconds;
+        }
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        // base.OnMasterClientSwitched
+    }
+}
